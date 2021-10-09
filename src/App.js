63db651f9910a0914ebc74d602ae29e9e8aff203
Harshal Trivedi <harshal30897@gmail.com>
2021-10-09 11:44:40 -0700

    Changed identifiers.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [author, setAuthor] = useState("");
   //  state to manage click of button
   const [newQuote, setNewQuote] = useState(false);
+
   async function randomQuote() {
     const response = await fetch("https://api.quotable.io/random");
     const data = await response.json();
